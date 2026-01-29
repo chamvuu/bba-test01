d0f268a4fae4c0ca81bc15e22847ebbfe9ec5d09
cham vu <chamvudqh@gmail.com>
2026-01-29 22:34:37 +0700

    add solution for challenge 03

---
+++ tests/tests/lesson-04/cloud-kingdom.js
@@ -0,0 +1,13 @@
+let powerUp = "mushroom";
+
+if (powerUp === "mushroom"){
+    console.log('Mario becomes Super!');
+}else if(powerUp === "flower"){
+    console.log('Mario can shoot fireball!');
+}else if(powerUp === "star"){
+    console.log('Mario is invincible!');
+}else if(powerUp === "none"){
+    console.log('Mario is normal!');
+}else{
+    console.log('Unknown power up!');
+}
